664c71639a94e9faef06f76e916e875f55c5c82e
rajeev <rajeev.shetty24@gmail.com>
2024-07-26 23:33:07 +0800

    Added additional test cases and logic code refactoring or longest binary index challenge

--- src/main/java/longestbinarystring/LongestBinaryString.java
+++ src/main/java/longestbinarystring/LongestBinaryString.java
@@ -1,7 +1,64 @@
 package longestbinarystring;
 
 public class LongestBinaryString {
-    public int findStartIndexOfLongestZeroOrOne(Integer input, int binaryDigit) {
-        throw new RuntimeException();
+    public int findStartIndexOfLongestZeroOrOne(Integer input, char binaryDigit) {
+        int maxIndex = -1;
+
+        if(!isValidateInput(input,binaryDigit))
+            return maxIndex;
+
+        String binaryString = convertDecimalToBinary(input);
+        int binaryStringLength = binaryString.length();
+        int runningIndex = 0;
+        int maxCount = 0; //count of current max
+        int currentStartIndex = 0; //This would be the start anchor in scan.
+
+
+        while (currentStartIndex < binaryStringLength) {
+            currentStartIndex = runningIndex;
+
+            //This is to position the startIndex to the next occurrence of binaryDigit and skip non-matching digit.
+            currentStartIndex = getNextStartIndex(binaryDigit, currentStartIndex, binaryStringLength, binaryString);
+
+            runningIndex = currentStartIndex;
+            int currentCount = 0;
+            //This construct counts the matching digits.
+            while ((runningIndex < binaryStringLength) && (binaryString.charAt(runningIndex) == binaryDigit)) {
+                ++currentCount;
+                ++runningIndex;
+            }
+
+            if (currentCount > maxCount) {
+                maxIndex = currentStartIndex;
+                maxCount = currentCount;
+            }
+        }
+
+        //Edge case for no matches found (No matching digits).
+        if (maxIndex != -1) return maxIndex + 1;
+        return maxIndex;
     }
+
+    private boolean isValidateInput(Integer input, char binaryDigit) {
+        if(input < 0) return false;
+        return binaryDigit == '1' || binaryDigit == '0';
+    }
+
+    private static int getNextStartIndex(char binaryDigit, int currentStartIndex, int binaryStringLength, String binaryString) {
+        while ((currentStartIndex < binaryStringLength) && (binaryString.charAt(currentStartIndex) != binaryDigit))
+            ++currentStartIndex;
+        return currentStartIndex;
+    }
+
+    private String convertDecimalToBinary(Integer input) {
+        if (input == 0)  return "0";
+
+        StringBuilder binaryBuffer = new StringBuilder();
+        while(input != 0) {
+            binaryBuffer.append(input % 2);
+            input = input / 2;
+        }
+        return binaryBuffer.reverse().toString();
+    }
+
 }
--- src/test/java/LongestBinaryStringTest.java
+++ src/test/java/LongestBinaryStringTest.java
@@ -4,11 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 
 @DisplayName("Assertions to check start of longest string with 1s in a binary number")
 public class LongestBinaryStringTest {
@@ -21,25 +17,55 @@ public class LongestBinaryStringTest {
     }
 
     @Test
-    public void givenANumber_whenConvertedToBinary_thenReturnStartPositionOfMax1Length() {
+    public void givenPositiveNumber156SearchBinary1_whenConvertedToBinary_thenSuccessReturn4() {
         int inputDecimalNumber = 156;
-        int startIndex = longestBinaryString.findStartIndexOfLongestZeroOrOne(inputDecimalNumber,1);
+        int startIndex = longestBinaryString.findStartIndexOfLongestZeroOrOne(inputDecimalNumber,'1');
         assert(Integer.valueOf(startIndex)).equals(4);
     }
 
+
     @Test
-    public void givenANumber_whenConvertedToBinary_thenReturnStartPositionOfMax0Length() {
+    public void givenPositiveNumber88SearchBinary1_whenConvertedToBinary_thenSuccessReturn3() {
+        int inputDecimalNumber = 88;
+        int startIndex = longestBinaryString.findStartIndexOfLongestZeroOrOne(inputDecimalNumber,'1');
+        assert(Integer.valueOf(startIndex)).equals(3);
+    }
+
+    @Test
+    public void givenPositiveNumber156SearchBinary0_whenConvertedToBinary_thenSuccessReturn2() {
         int inputDecimalNumber = 156;
-        int startIndex = longestBinaryString.findStartIndexOfLongestZeroOrOne(inputDecimalNumber,0);
+        int startIndex = longestBinaryString.findStartIndexOfLongestZeroOrOne(inputDecimalNumber,'0');
         assert(Integer.valueOf(startIndex)).equals(2);
     }
 
     @Test
-    public void givenANumber_whenConvertedToBinaryAndNo1Found_thenReturnNegativeNumber() {
+    public void givenPositiveNumber0SearchBinary0_whenConvertedToBinary_thenSuccessReturn1() {
         int inputDecimalNumber = 0;
-        int startIndex = longestBinaryString.findStartIndexOfLongestZeroOrOne(inputDecimalNumber,1);
+        int startIndex = longestBinaryString.findStartIndexOfLongestZeroOrOne(inputDecimalNumber,'0');
+        assert(Integer.valueOf(startIndex)).equals(1);
+    }
+
+
+    //Negative cases
+    @Test
+    public void givenPositiveNumber0SearchBinary1_whenConvertedToBinary_thenFailReturnNegativeIndex() {
+        int inputDecimalNumber = 0;
+        int startIndex = longestBinaryString.findStartIndexOfLongestZeroOrOne(inputDecimalNumber,'1');
         assertThat(Integer.valueOf(startIndex)).isLessThan(0);
     }
 
+    @Test
+    public void givenNegativeNumber_whenValidatedAndError_thenFailReturnNegativeIndex() {
+        int inputDecimalNumber = -10;
+        int startIndex = longestBinaryString.findStartIndexOfLongestZeroOrOne(inputDecimalNumber,'1');
+        assertThat(Integer.valueOf(startIndex)).isLessThan(0);
+    }
+
+    @Test
+    public void givenPositiveNumberAndInvalidBinaryDigit_whenValidatedAndError_thenFailReturnNegativeIndex() {
+        int inputDecimalNumber = -10;
+        int startIndex = longestBinaryString.findStartIndexOfLongestZeroOrOne(inputDecimalNumber,'2');
+        assertThat(Integer.valueOf(startIndex)).isLessThan(0);
+    }
 
 }
